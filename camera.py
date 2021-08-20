39194e40be65ceaa010d6cb3de2855027eabf1f2
Tatmius <kddiw.comusodayo@gmail.com>
2021-08-21 08:29:39 +0900

    migrate camera class into 1 class

--- camera.py
+++ camera.py
@@ -11,84 +11,84 @@ import atexit
 
 class VideoCamera(object):
     def __init__(self, resolution=(1280, 720), framerate=32, flipVert = False, flipHor = False, **kwargs):
-				self.camera = PiCamera()
-				self.camera.resolution = resolution
-				self.camera.framerate = framerate
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
 
-				for (arg, value) in kwargs.items():
-					setattr(self.camera, arg, value)
+        for (arg, value) in kwargs.items():
+          setattr(self.camera, arg, value)
 
-				# initialize the stream
-				self.rawCapture = PiRGBArray(self.camera, size=resolution)
-				self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
+        # initialize the stream
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
 
-				self.frame = None
-				self.stopped = False
+        self.frame = None
+        self.stopped = False
 
-				self.vs = self.start()
-				self.flipVert = flipVert
-				self.flipHor = flipHor
+        self.vs = self.start()
+        self.flipVert = flipVert
+        self.flipHor = flipHor
 
-				atexit.register(self.close_camera)
+        atexit.register(self.close_camera)
 
-				time.sleep(2.0)
-				
-		def start(self):
-				# start the thread to read frames from the video stream
-				t = Thread(target=self.update, args=())
-				t.daemon = True
-				t.start()
-				return self
-		
-		def update(self):
-			# keep looping infinitely until the thread is stopped
-			for f in self.stream:
-				# grab the frame from the stream and clear the stream in
-				# preparation for the next frame
-				self.frame = f.array
-				self.rawCapture.truncate(0)
+        time.sleep(2.0)
+    
+    def start(self):
+        # start the thread to read frames from the video stream
+        t = Thread(target=self.update, args=())
+        t.daemon = True
+        t.start()
+        return self
+    
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        for f in self.stream:
+          # grab the frame from the stream and clear the stream in
+          # preparation for the next frame
+          self.frame = f.array
+          self.rawCapture.truncate(0)
 
-				# if the thread indicator variable is set, stop the thread
-				# and resource camera resources
-				if self.stopped:
-					self.stream.close()
-					self.rawCapture.close()
-					self.camera.close()
-					return
+          # if the thread indicator variable is set, stop the thread
+          # and resource camera resources
+          if self.stopped:
+            self.stream.close()
+            self.rawCapture.close()
+            self.camera.close()
+            return
 
-		def __del__(self):
+    def __del__(self):
         self.vs.stop()
-				self.camera.close()
+        self.camera.close()
 
-		def read(self):
-			return self.frame
+    def read(self):
+        return self.frame
 
-		def stop(self):
-				self.stopped = True
+    def stop(self):
+        self.stopped = True
 
-		def restart(self):
-				self.stopped = False
-	
-		def close_camera(self):
-				self.camera.close()
+    def restart(self):
+        self.stopped = False
+  
+    def close_camera(self):
+        self.camera.close()
 
     def flip_vert(self, frame):
-				#flip video vertically
+        #flip video vertically
         if self.flipVert:
             return np.flip(frame, 0)
         return frame
 
     def flip_hor(self, frame):
-				#flip video horizontally
+        #flip video horizontally
         if self.flipHor:
             return np.flip(frame, 1)
         return frame
-		
+    
     def rotate(self, frame):
-		    return np.rot90(frame)
-		
-		def get_frame(self):
-				frame = self.rotate(self.flip_hor(self.flip_vert(self.vs.read())))
-				ret, jpeg = cv2.imencode('.jpg', frame)
-				return jpeg.tobytes()
+        return np.rot90(frame)
+    
+    def get_frame(self):
+        frame = self.rotate(self.flip_hor(self.flip_vert(self.vs.read())))
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
 
